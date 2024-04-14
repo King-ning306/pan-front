1aaecafdd36e29f3282904d1eb3924f8135c5e59
张宁 <1219951956@qq.com>
2024-04-14 11:10:37 +0800

    文件重命名

--- src/main.js
+++ src/main.js
@@ -9,18 +9,23 @@ import Verify from'@/utils/Verify'
 import Message from'@/utils/Message'
 import Request from './utils/Request'
 import Confirm from './utils/Confirm'
+import Utils from '@/utils/Utils'
 //引入cookies
 import VueCookies from 'vue-cookies'
 //自定义组件
 import Dialog from '@/components/Dialog.vue'
 import Avatar from '@/components/Avatar.vue'
+import Table from '@/components/Table.vue'
+import Icon from '@/components/Icon.vue'
 
 
 const app = createApp(App)
 app.use(ElementPlus)
 app.component("Dialog",Dialog)
-
 app.component("Avatar",Avatar)
+app.component("Table",Table)
+app.component("Icon",Icon)
+
 app.use(router)
 //配置全局组件
 app.config.globalProperties.Confirm=Confirm
@@ -28,7 +33,10 @@ app.config.globalProperties.Verify=Verify
 app.config.globalProperties.Message=Message
 app.config.globalProperties.Request=Request
 app.config.globalProperties.VueCookies=VueCookies
+app.config.globalProperties.Utils=Utils
+
 app.config.globalProperties.globalInfo={
-     avatarUrl:"/api/getAvatar/"
+     avatarUrl:"/api/getAvatar/",
+     imageUrl:"/api/file/getImage/"
 }
 app.mount('#app')
--- src/views/Framework.vue
+++ src/views/Framework.vue
@@ -1,94 +1,127 @@
 <template>
-    <div class="framework">
-      <div class="header">
-        <div class="logo">
-          <span class="iconfont icon-pan"></span>
-          <div class="name">Easy云盘</div>
-        </div>
-        <div class="right-panel">
-          <el-popover
-            :v-model:visible="true"
-            :offset="20"
-            transition="none"
-            :hide-after="0"
-            :popper-style="{padding: '0px'}"
-          >
-            <template #reference>
-              <span class="iconfont icon-transfer"></span>
-            </template>
-            <template #default>这里是显示区域</template>
-          </el-popover>
-          <el-dropdown>
-            <div class="user-info">
-              <div class="avatar">
-                <Avatar :userId="userInfo.userId" :avatar="userInfo.avatar" :timestamp="timestamp" :width="46"></Avatar>
-              </div>
-              <span class="nick-name">{{ userInfo.nickName }}</span>
-              
-            </div>
-            <template #dropdown>
-                <el-dropdown-menu>
-                  <el-dropdown-item @click="updateAvatar">修改头像</el-dropdown-item>
-                  <el-dropdown-item @click="updatePassword">修改密码</el-dropdown-item>
-                  <el-dropdown-item @click="logout">退出登录</el-dropdown-item>
-                </el-dropdown-menu>
-              </template>
-          </el-dropdown>
-        </div>
+  <div class="framework">
+    <div class="header">
+      <div class="logo">
+        <span class="iconfont icon-pan"></span>
+        <div class="name">Easy云盘</div>
       </div>
-      <div class="body">
-        <div class="left-sider">
-            <div class="menu-list">
-                <div :class="['menu-item' ,
-                item.menuCode==currentMenu.menuCode?'active':'']"
-                v-for="item in menus" @click="jump(item)"
-                
-                >
-                   <div :class="['iconfont','icon-'+item.icon]"></div>
-                   <div class="text">{{ item.name }}</div>
-                </div>
+      <div class="right-panel">
+        <el-popover
+          v-model:visible="showUploader"
+          :offset="20"
+          transition="none"
+          :hide-after="0"
+          :popper-style="{ padding: '0px' }"
+        >
+          <template #reference>
+            <span class="iconfont icon-transfer"></span>
+          </template>
+          <template #default>
+          <Uploader ref="uploaderRef"></Uploader>
+          </template>
+        </el-popover>
+        <el-dropdown>
+          <div class="user-info">
+            <div class="avatar">
+              <Avatar
+                :userId="userInfo.userId"
+                :avatar="userInfo.avatar"
+                :timestamp="timestamp"
+                :width="46"
+              ></Avatar>
             </div>
-            <div class="menu-sub-list">
-               <div :class="['menu-item-sub',currentPath==sub.path?'active':'']" v-for="sub in currentMenu.children" @click="jump(sub)">
-                <span :class="['iconfont','icon-'+sub.icon]" v-if="sub.icon">
-                </span>
-                <span class="text">{{ sub.name }}</span>
-            </div>
-               <div class="tips" v-if="currentMenu&&currentMenu.tips">{{ currentMenu.tips }} </div>
-               <div class="space-info">
-                <div>空间使用</div>
-                <div class="percent">
-
-                </div>
-               </div>
-            </div>
-        </div>
-        <div class="body-content">
-          <router-view v-slot="{Component}">
-            <component :is="Component"></component>
-          </router-view>
-        </div>
-
+            <span class="nick-name">{{ userInfo.nickName }}</span>
+          </div>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item @click="updateAvatar"
+                >修改头像</el-dropdown-item
+              >
+              <el-dropdown-item @click="updatePassword"
+                >修改密码</el-dropdown-item
+              >
+              <el-dropdown-item @click="logout">退出登录</el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
       </div>
-      <UpdateAvatar @updateAvatar="reloadAvatar" ref="updateAvatarRef"></UpdateAvatar>
-      <UpdatePassword ref="updatePasswordRef"></UpdatePassword>
     </div>
-  </template>
+    <div class="body">
+      <div class="left-sider">
+        <div class="menu-list">
+          <div
+            :class="[
+              'menu-item',
+              item.menuCode == currentMenu.menuCode ? 'active' : '',
+            ]"
+            v-for="item in menus"
+            @click="jump(item)"
+          >
+            <div :class="['iconfont', 'icon-' + item.icon]"></div>
+            <div class="text">{{ item.name }}</div>
+          </div>
+        </div>
+        <div class="menu-sub-list">
+          <div
+            :class="['menu-item-sub', currentPath == sub.path ? 'active' : '']"
+            v-for="sub in currentMenu.children"
+            @click="jump(sub)"
+          >
+            <span :class="['iconfont', 'icon-' + sub.icon]" v-if="sub.icon">
+            </span>
+            <span class="text">{{ sub.name }}</span>
+          </div>
+          <div class="tips" v-if="currentMenu && currentMenu.tips">
+            {{ currentMenu.tips }}
+          </div>
+          <div class="space-info">
+            <div>空间使用</div>
+            <div class="percent"></div>
+          </div>
+        </div>
+      </div>
+      <div class="body-content">
+        <router-view v-slot="{ Component }">
+          <component :is="Component" @addFile="addFile"></component>
+        </router-view>
+      </div>
+    </div>
+    <UpdateAvatar
+      @updateAvatar="reloadAvatar"
+      ref="updateAvatarRef"
+    ></UpdateAvatar>
+    <UpdatePassword ref="updatePasswordRef"></UpdatePassword>
+  </div>
+</template>
   
 <script setup>
+import Uploader from "@/views/main/Uploader.vue";
 import UpdatePassword from "./UpdatePassword.vue";
-import UpdateAvatar from '@/views/UpdateAvatar.vue'
-import{ref,reactive,getCurrentInstance,nextTick, watch}from "vue";
-const {proxy}=getCurrentInstance();
-import {useRouter,useRoute} from "vue-router";
-const router= useRouter();
-const route=useRoute();
-const api={
+import UpdateAvatar from "@/views/UpdateAvatar.vue";
+import { ref, reactive, getCurrentInstance, nextTick, watch } from "vue";
+const { proxy } = getCurrentInstance();
+
+
+import { useRouter, useRoute } from "vue-router";
+const router = useRouter();
+const route = useRoute();
+const api = {
   logout: "/logout",
+};
+//显示上传区域
+const showUploader = ref(false);
+//添加文件
+const uploaderRef=ref();
+const addFile=(data)=>{
+  const {file,filePid}=data;
+  showUploader.value=true;
+  uploaderRef.value.addFile(file,filePid);
+  
 }
 
 
-const userInfo=ref(proxy.VueCookies.get("userInfo"));
+
+const userInfo = ref(proxy.VueCookies.get("userInfo"));
 const menus = [
   {
     icon: "cloude",
@@ -184,30 +217,30 @@ const menus = [
     ],
   },
 ];
-const timestamp=ref(0);
-const currentMenu=ref({});
-const currentPath=ref();
-const jump=(data)=>{
-    if(!data.path||data.menuCode==currentMenu.value.menuCode){
-        return;
-    }
-    router.push(data.path);
+const timestamp = ref(0);
+const currentMenu = ref({});
+const currentPath = ref();
+const jump = (data) => {
+  if (!data.path || data.menuCode == currentMenu.value.menuCode) {
+    return;
+  }
+  router.push(data.path);
 };
-const setMenu =(menuCode,path)=>{
-    const menu=menus.find(item=>{
-        return item.menuCode===menuCode;
-    });
-    currentMenu.value=menu;
-    currentPath.value==path;
-    
-}
+const setMenu = (menuCode, path) => {
+  const menu = menus.find((item) => {
+    return item.menuCode === menuCode;
+  });
+  currentMenu.value = menu;
+  currentPath.value == path;
+};
 watch(
-    ()=>route,(newVal,oldVal)=>{
-    if(newVal.meta.menuCode){
-        setMenu(newVal.meta.menuCode,newVal.path);
+  () => route,
+  (newVal, oldVal) => {
+    if (newVal.meta.menuCode) {
+      setMenu(newVal.meta.menuCode, newVal.path);
     }
-},
-{immediate:true,deep:true}
+  },
+  { immediate: true, deep: true }
 );
 //退出登录
 const logout = () => {
@@ -223,12 +256,10 @@ const logout = () => {
   });
 };
 //上传头像
-const updateAvatarRef=ref();
-const updateAvatar=()=>{
-  updateAvatarRef.value.show(userInfo.value)
-
-
-}
+const updateAvatarRef = ref();
+const updateAvatar = () => {
+  updateAvatarRef.value.show(userInfo.value);
+};
 const reloadAvatar = () => {
   userInfo.value = proxy.VueCookies.get("userInfo");
   timestamp.value = new Date().getTime();
@@ -238,180 +269,179 @@ const updatePasswordRef = ref();
 const updatePassword = () => {
   updatePasswordRef.value.show();
 };
-
 </script>
 <style lang="scss" scoped>
 .header {
-    box-shadow: 0 3px 10px 0 rgb(0 0 0 / 6%);
-    height: 56px;
-    padding-left: 24px;
-    padding-right: 24px;
-    position: relative;
-    z-index: 200;
+  box-shadow: 0 3px 10px 0 rgb(0 0 0 / 6%);
+  height: 56px;
+  padding-left: 24px;
+  padding-right: 24px;
+  position: relative;
+  z-index: 200;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .logo {
     display: flex;
     align-items: center;
-    justify-content: space-between;
 
-    .logo {
-        display: flex;
-        align-items: center;
-
-        .icon-pan {
-            font-size: 40px;
-            color: #1296db;
-        }
-
-        .name {
-            font-weight: bold;
-            margin-left: 5px;
-            font-size: 25px;
-            color: #05a1f5;
-        }
+    .icon-pan {
+      font-size: 40px;
+      color: #1296db;
     }
 
-    .right-panel {
-        display: flex;
-        align-items: center;
-
-        .icon-transfer {
-            cursor: pointer;
-        }
-
-        .user-info {
-            margin-right: 10px;
-            display: flex;
-            align-items: center;
-            cursor: pointer;
-
-            .avatar {
-                margin: 0px 5px 0px 15px;
-            }
-
-            .nick-name {
-                color: #05a1f5;
-            }
-        }
+    .name {
+      font-weight: bold;
+      margin-left: 5px;
+      font-size: 25px;
+      color: #05a1f5;
     }
+  }
+
+  .right-panel {
+    display: flex;
+    align-items: center;
+
+    .icon-transfer {
+      cursor: pointer;
+    }
+
+    .user-info {
+      margin-right: 10px;
+      display: flex;
+      align-items: center;
+      cursor: pointer;
+
+      .avatar {
+        margin: 0px 5px 0px 15px;
+      }
+
+      .nick-name {
+        color: #05a1f5;
+      }
+    }
+  }
 }
 
 .body {
+  display: flex;
+
+  .left-sider {
+    border-right: 1px solid #f1f2f4;
     display: flex;
 
-    .left-sider {
-        border-right: 1px solid #f1f2f4;
-        display: flex;
+    .menu-list {
+      height: calc(100vh - 56px);
+      width: 80px;
+      box-shadow: 0 3px 10px 0 rgb(0 0 0 / 6%);
+      border-right: 1px solid #f1f2f4;
 
-        .menu-list {
-            height: calc(100vh - 56px);
-            width: 80px;
-            box-shadow: 0 3px 10px 0 rgb(0 0 0 / 6%);
-            border-right: 1px solid #f1f2f4;
+      .menu-item {
+        text-align: center;
+        font-size: 14px;
+        font-weight: bold;
+        padding: 20px 0px;
+        cursor: pointer;
 
-            .menu-item {
-                text-align: center;
-                font-size: 14px;
-                font-weight: bold;
-                padding: 20px 0px;
-                cursor: pointer;
-
-                &:hover {
-                    background: #f3f3f3;
-                }
-
-                .iconfont {
-                    font-weight: normal;
-                    font-size: 28px;
-                }
-            }
-
-            .active {
-                .iconfont {
-                    color: #06a7ff;
-                }
-
-                .text {
-                    color: #06a7ff;
-                }
-            }
+        &:hover {
+          background: #f3f3f3;
         }
 
-        .menu-sub-list {
-            width: 200px;
-            padding: 20px 10px 0px;
-            position: relative;
-
-            .menu-item-sub {
-                text-align: center;
-                line-height: 40px;
-                border-radius: 5px;
-                cursor: pointer;
-
-                &:hover {
-                    background: #f3f3f3;
-                }
-
-                .iconfont {
-                    font-size: 14px;
-                    margin-right: 20px;
-                }
-
-                .text {
-                    font-size: 13px;
-                }
-            }
-
-            .active {
-                background: #eef9fe;
-
-                .iconfont {
-                    color: #05a1f5;
-                }
-
-                .text {
-                    color: #05a1f5;
-                }
-            }
-
-            .tips {
-                margin-top: 10px;
-                color: #888888;
-                font-size: 13px;
-            }
-
-            .space-info {
-                position: absolute;
-                bottom: 10px;
-                width: 100%;
-                padding: 0px 5px;
-
-                .percent {
-                    padding-right: 10px;
-                }
-
-                .space-use {
-                    margin-top: 5px;
-                    color: #7e7e7e;
-                    display: flex;
-                    justify-content: space-around;
-
-                    .use {
-                        flex: 1;
-                    }
-
-                    .iconfont {
-                        cursor: pointer;
-                        margin-right: 20px;
-                        color: #05a1f5;
-                    }
-                }
-            }
+        .iconfont {
+          font-weight: normal;
+          font-size: 28px;
         }
+      }
+
+      .active {
+        .iconfont {
+          color: #06a7ff;
+        }
+
+        .text {
+          color: #06a7ff;
+        }
+      }
     }
 
-    .body-content {
-        flex: 1;
-        width: 0;
-        padding-left: 20px;
+    .menu-sub-list {
+      width: 200px;
+      padding: 20px 10px 0px;
+      position: relative;
+
+      .menu-item-sub {
+        text-align: center;
+        line-height: 40px;
+        border-radius: 5px;
+        cursor: pointer;
+
+        &:hover {
+          background: #f3f3f3;
+        }
+
+        .iconfont {
+          font-size: 14px;
+          margin-right: 20px;
+        }
+
+        .text {
+          font-size: 13px;
+        }
+      }
+
+      .active {
+        background: #eef9fe;
+
+        .iconfont {
+          color: #05a1f5;
+        }
+
+        .text {
+          color: #05a1f5;
+        }
+      }
+
+      .tips {
+        margin-top: 10px;
+        color: #888888;
+        font-size: 13px;
+      }
+
+      .space-info {
+        position: absolute;
+        bottom: 10px;
+        width: 100%;
+        padding: 0px 5px;
+
+        .percent {
+          padding-right: 10px;
+        }
+
+        .space-use {
+          margin-top: 5px;
+          color: #7e7e7e;
+          display: flex;
+          justify-content: space-around;
+
+          .use {
+            flex: 1;
+          }
+
+          .iconfont {
+            cursor: pointer;
+            margin-right: 20px;
+            color: #05a1f5;
+          }
+        }
+      }
     }
+  }
+
+  .body-content {
+    flex: 1;
+    width: 0;
+    padding-left: 20px;
+  }
 }
 </style>
